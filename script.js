38870b635e9c6b9686554a7fe74bd4411ced750e
Albina Starykova <albina.starikova@gmail.com>
2022-06-07 23:01:37 +0300

    Add function playRound

--- script.js
+++ script.js
@@ -3,15 +3,61 @@
 // getting computer's input
 
 function computerPlay() {
-    computer_input = Math.floor(Math.random() * 3) + 1;
-    return computer_input;
+    let random = Math.floor(Math.random() * 3) + 1;
+
+    if (random == 1) {
+        let computer_input = "ROCK";
+        return computer_input;
+    } else if (random == 2) {
+        let computer_input = "PAPER";
+        return computer_input;
+    } else {
+        let computer_input = "SCISSORS";
+        return computer_input;
+    }    
+
 };
-computerPlay();
-console.log(computerPlay);
 
+let computerSelection = computerPlay();
+console.log(computerSelection);
 
+// getting user's input
 
-// compare user's and computer's input
+const playerSelection = "PApER";
 
-// announce the winner
+// single play round - compare user's and computer's input
+// for computer selection 1 = rock, 2 = paper, 3 = scissors 
+
+function playRound(playerSelection, computerSelection) {
+    // computer wins
+    if (computerSelection == "ROCK" && playerSelection.toUpperCase() == "SCISSORS") {
+        let message = "You lose! Rock beats scissors";
+        return message;
+    } else if (computerSelection == "PAPER" && playerSelection.toUpperCase() == "ROCK") {
+        let message = "You lose! Paper beats rock";
+        return message;
+    } else if (computerSelection == "SCISSORS" && playerSelection.toUpperCase() == "PAPER") { 
+        let message = "You lose! Scissors beats paper";
+        return message;
+    } 
+    // user wins
+    else if (computerSelection == "ROCK" && playerSelection.toUpperCase() == "PAPER") {
+        let message = "You win! Paper beats rock";
+        return message;
+    } else if (computerSelection == "PAPER" && playerSelection.toUpperCase() == "SCISSORS") {
+        let message = "You win! Scissors beats paper"; 
+        return message;     
+    } else if (computerSelection == "SCISSORS" && playerSelection.toUpperCase() == "ROCK") {
+        let message = "You win! Rock beats scissors"; 
+        return message;
+    }
+    // tie
+    else {
+        let message = "It's a tie!";
+        return message;
+    }
+}
+
+playRound(playerSelection, computerSelection);  
+console.log(playRound(playerSelection, computerSelection));
 
