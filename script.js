2994f31e4d6b5e7a81fa930ba6c8a98e64a837c5
Albina Starykova <albina.starikova@gmail.com>
2022-06-07 21:05:57 +0300

    Add computerPlay function

---
+++ script.js
@@ -0,0 +1,17 @@
+// getting user's input
+
+// getting computer's input
+
+function computerPlay() {
+    computer_input = Math.floor(Math.random() * 3) + 1;
+    return computer_input;
+};
+computerPlay();
+console.log(computerPlay);
+
+
+
+// compare user's and computer's input
+
+// announce the winner
+
