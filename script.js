86b64c8f149aaf2fdaf0ebca215d6a06480615e2
Albina Starykova <albina.starikova@gmail.com>
2022-06-08 17:51:18 +0300

    Add game function

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// getting user's input
-
-// getting computer's input
-
+// function on getting computer's input
 function computerPlay() {
     let random = Math.floor(Math.random() * 3) + 1;
 
@@ -18,46 +15,83 @@ function computerPlay() {
 
 };
 
-let computerSelection = computerPlay();
-console.log(computerSelection);
+// function on simulating a single play round
+function playRound() {
 
-// getting user's input
+    // getting user's input
+    let playerSelection = prompt("Rock, paper, or scissors?");
+    console.log(playerSelection); 
+    
+    // getting computer's input
+    let computerSelection = computerPlay();
+    console.log(computerSelection); 
 
-const playerSelection = "PApER";
-
-// single play round - compare user's and computer's input
-// for computer selection 1 = rock, 2 = paper, 3 = scissors 
-
-function playRound(playerSelection, computerSelection) {
-    // computer wins
+    // check if computer wins
     if (computerSelection == "ROCK" && playerSelection.toUpperCase() == "SCISSORS") {
-        let message = "You lose! Rock beats scissors";
-        return message;
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        let roundResult = "computer";
+        return roundResult;
     } else if (computerSelection == "PAPER" && playerSelection.toUpperCase() == "ROCK") {
-        let message = "You lose! Paper beats rock";
-        return message;
+        console.log( `You lose! ${computerSelection} beats ${playerSelection}`);
+        let roundResult = "computer";
+        return roundResult;
     } else if (computerSelection == "SCISSORS" && playerSelection.toUpperCase() == "PAPER") { 
-        let message = "You lose! Scissors beats paper";
-        return message;
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        let roundResult = "computer";
+        return roundResult;
     } 
-    // user wins
+    // check if user wins
     else if (computerSelection == "ROCK" && playerSelection.toUpperCase() == "PAPER") {
-        let message = "You win! Paper beats rock";
-        return message;
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        let roundResult = "user";
+        return roundResult;
     } else if (computerSelection == "PAPER" && playerSelection.toUpperCase() == "SCISSORS") {
-        let message = "You win! Scissors beats paper"; 
-        return message;     
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        let roundResult = "user";
+        return roundResult;   
     } else if (computerSelection == "SCISSORS" && playerSelection.toUpperCase() == "ROCK") {
-        let message = "You win! Rock beats scissors"; 
-        return message;
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        let roundResult = "user";
+        return roundResult;
     }
-    // tie
+    // check if it's a tie
     else {
-        let message = "It's a tie!";
-        return message;
+        console.log("It's a tie!");
+        let roundResult = "tie";
+        return roundResult;
     }
 }
 
-playRound(playerSelection, computerSelection);  
-console.log(playRound(playerSelection, computerSelection));
+// function on simulating a game of 5 rounds
+function game () {
+
+    // setting counters of both user's and computer's wins
+    let userCounter = 0;
+    let computerCounter = 0;
+
+    // running 5 rounds of a game
+    for (let i = 0; i < 5; i++) {
+        let roundResult = playRound(); 
+
+        if(roundResult == "user") {
+            userCounter += 1;   
+        } else if (roundResult == "computer") {  
+            computerCounter += 1;
+        } 
+    }
+
+    // checking who is the winner of the game
+    if (userCounter > computerCounter) {
+        console.log("You won this game!");
+    } else if (userCounter < computerCounter) {
+        console.log("You lost this game!");
+    } else {
+        console.log("It's a tie!");
+    }
+
+}
+
+// simulating a game of 5 rounds
+game();
+
 
