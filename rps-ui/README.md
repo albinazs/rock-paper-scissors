5d7c85024488c296f970075b048f68120da421db
Albina Starykova <albina.starikova@gmail.com>
2022-06-15 19:17:14 +0300

    Edit files structure

--- rps-ui/README.md
+++
@@ -1,2 +0,0 @@
-# rock-paper-scissors
-This is a rock-paper-scissors console game implementation to show my basic Javascript skills.
--- script.js
+++ script.js
@@ -1,110 +1,157 @@
-// TO DO replace with switch/break and wo var - just return
-// try to use array instead?
+let computerCounter = 0;
+let playerCounter = 0;
+let roundResult = '';
+
+// function on simulating a single play round
+function playRound(playerSelection) {
+
+    const computerSelection = computerPlay();
+
+    if ((computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+    (computerSelection === "PAPER" && playerSelection === "ROCK")  ||
+    (computerSelection === "SCISSORS" && playerSelection === "PAPER")
+    ) {
+        roundResult = "computer";
+        computerCounter++;
+    }
+    else if ((computerSelection === "ROCK" && playerSelection === "PAPER") ||
+    (computerSelection === "PAPER" && playerSelection === "SCISSORS") ||
+    (computerSelection === "SCISSORS" && playerSelection === "ROCK")
+    ) {
+        roundResult = "player";
+        playerCounter++;
+    } else if (playerSelection === computerSelection) {
+        roundResult = "tie";
+    }
+    updateChoice(computerSelection, playerSelection);
+    getMessage(roundResult, computerSelection, playerSelection)
+
+    if (gameOver()) {
+        openEndgameModal()
+        setFinalMessage()
+      }
+}
 
 // function on getting computer's input
 function computerPlay() {
     let random = Math.floor(Math.random() * 3) + 1;
-
     if (random == 1) {
-        let computer_input = "ROCK"; //return "ROCK"
-        return computer_input;
+        return "ROCK"; 
     } else if (random == 2) {
-        let computer_input = "PAPER";
-        return computer_input;
+        return "PAPER";
     } else {
-        let computer_input = "SCISSORS";
-        return computer_input;
+        return "SCISSORS";
     }    
-
 };
 
-// function on simulating a single play round
-function playRound() {
+function updateChoice (computerSelection, playerSelection) {
 
-    // getting user's input
-    let playerSelection = prompt("Rock, paper, or scissors?");
-    console.log(playerSelection); 
-    
-    // getting computer's input
-    let computerSelection = computerPlay();
-    console.log(computerSelection); 
+    pChoicePic.textContent = "";
+    cChoicePic.textContent = "";
 
-    // check if computer wins
-    if (computerSelection == "ROCK" && playerSelection.toUpperCase() == "SCISSORS") {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-        let roundResult = "computer";
-        return roundResult;
-    } else if (computerSelection == "PAPER" && playerSelection.toUpperCase() == "ROCK") {
-        console.log( `You lose! ${computerSelection} beats ${playerSelection}`);
-        let roundResult = "computer";
-        return roundResult;
-    } else if (computerSelection == "SCISSORS" && playerSelection.toUpperCase() == "PAPER") { 
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-        let roundResult = "computer";
-        return roundResult;
-    } 
-    // check if user wins
-    else if (computerSelection == "ROCK" && playerSelection.toUpperCase() == "PAPER") {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        let roundResult = "user";
-        return roundResult;
-    } else if (computerSelection == "PAPER" && playerSelection.toUpperCase() == "SCISSORS") {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        let roundResult = "user";
-        return roundResult;   
-    } else if (computerSelection == "SCISSORS" && playerSelection.toUpperCase() == "ROCK") {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        let roundResult = "user";
-        return roundResult;
+    switch (computerSelection) {
+        case "ROCK":
+            cChoicePic.appendChild(rockPic);
+            break;
+        case "PAPER":
+            cChoicePic.appendChild(paperPic);
+            break;
+        case "SCISSORS":
+            cChoicePic.appendChild(scissorsPic);
+            break;
     }
-    // check if it's a tie
-    else {
-        console.log("It's a tie!");
-        let roundResult = "tie";
-        return roundResult;
+
+    switch (playerSelection) {
+        case "ROCK":
+            pChoicePic.appendChild(rockPic.cloneNode(true));
+            break;
+        case "PAPER":
+            pChoicePic.appendChild(paperPic.cloneNode(true));
+            break;
+        case "SCISSORS":
+            pChoicePic.appendChild(scissorsPic.cloneNode(true));
+            break;
     }
 }
 
-// function on simulating a game of 5 rounds
-function game () {
-
-    // setting counters of both user's and computer's wins
-    let userCounter = 0;
-    let computerCounter = 0;
-
-    // running 5 rounds of a game
-    for (let i = 0; i < 5; i++) {
-        let roundResult = playRound(); 
-
-        if(roundResult == "user") {
-            userCounter += 1;   
-        } else if (roundResult == "computer") {  
-            computerCounter += 1;
-        } 
+function getMessage(roundResult, computerSelection, playerSelection) {
+    if(roundResult === "computer") {
+     scoreMessage1.textContent = 'You lose!';
+     scoreMessage2.textContent = `${computerSelection} beats ${playerSelection}`;
+    } else if (roundResult === "player") {
+     scoreMessage1.textContent = 'You win!';
+     scoreMessage2.textContent = `${playerSelection} beats ${computerSelection}`;
+    } else if (roundResult === "tie") {
+     scoreMessage1.textContent = 'It\'s a tie!';
+     scoreMessage2.textContent = `${playerSelection} ties with ${computerSelection}`;
     }
+ 
+    computerScore.textContent = computerCounter;
+    playerScore.textContent = playerCounter;
+ }
 
-    // checking who is the winner of the game
-    if (userCounter > computerCounter) {
-        console.log("You won this game!");
-    } else if (userCounter < computerCounter) {
-        console.log("You lost this game!");
-    } else {
-        console.log("It's a tie!");
-    }
-
+function gameOver() {
+    return computerCounter === 5 || playerCounter === 5;
 }
 
-// simulating a game of 5 rounds
-game();
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+const scoreMessage1 = document.querySelector('h3');
+const scoreMessage2 = document.querySelector('.m');
+const cChoicePic = document.querySelector('.choice.c');
+const pChoicePic = document.querySelector('.choice.p');
+const computerScore = document.querySelector('.cs');
+const playerScore = document.querySelector('.ps');
+const endgameModal = document.querySelector('.endgameModal');
+const endgameMessage = document.querySelector('.endgameMessage');
+const overlay = document.querySelector('.overlay');
+const restartButton = document.querySelector('.restart');
 
+const rockPic = document.createElement('img');
+rockPic.setAttribute('src', './img/rock.png');
+rockPic.setAttribute('style', 'width: 130px;');
+const paperPic = document.createElement('img');
+paperPic.setAttribute('src', './img/paper.png');
+paperPic.setAttribute('style', 'width: 130px;');
+const scissorsPic = document.createElement('img');
+scissorsPic.setAttribute('src', './img/scissors.png');
+scissorsPic.setAttribute('style', 'width: 130px;');
 
-// <button onclick="myFunction()">Try it</button>
+const buttons = Array.from(document.querySelectorAll('button'));
+buttons.forEach(button => button.addEventListener('mouseover', (e) => e.target.classList.add('animated')));
+buttons.forEach(button => button.addEventListener('mouseout', (e) => e.target.classList.remove('animated')));
 
-<p id="demo"></p>
-<script>
-const fruits = ["Banana", "Orange", "Apple"];
-document.getElementById("demo").innerHTML = fruits;
-function myFunction() {
-  fruits.push("Lemon");
-  document.getElementById("demo").innerHTML = fruits;
-}
+rockButton.addEventListener('click', () => playRound('ROCK'));
+paperButton.addEventListener('click', () => playRound('PAPER'));
+scissorsButton.addEventListener('click', () => playRound('SCISSORS'));
+
+restartButton.addEventListener('click', restartGame)
+overlay.addEventListener('click', () => {
+    endgameModal.classList.remove('active')
+    overlay.classList.remove('active')
+});
+
+function openEndgameModal() {
+    endgameModal.classList.add('active')
+    overlay.classList.add('active')
+  }
+  
+  function setFinalMessage() {
+    return playerCounter > computerCounter
+      ? (endgameMessage.textContent = 'You won!')
+      : (endgameMessage.textContent = 'You lost!')
+  }
+  
+  function restartGame() {
+    playerCounter = 0;
+    computerCounter = 0;
+    scoreMessage1.textContent = 'Choose your weapon'
+    scoreMessage2.textContent = 'First to score 5 points wins the game'
+    pChoicePic.textContent = "?";
+    cChoicePic.textContent = "?";
+    computerScore.textContent = "0";
+    playerScore.textContent = "0";
+    endgameModal.classList.remove('active');
+    overlay.classList.remove('active');
+  }
